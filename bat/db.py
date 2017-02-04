6bc42d3a695fdd0f1a050ca0a1848311846bacb6
bofei <baifei7699@126.com>
2017-02-04 18:41:30 +0800

    1

--- bat/db.py
+++ bat/db.py
@@ -8,13 +8,40 @@ class db:
 
 	def __init__(self,conf):
 		self.__dbc_file = conf.DBC_FILE
-
+		self.__getDBC(self)
+		
+		
+	def __getDBC(self):
 		with open(self.__dbc_file, 'rt') as f:
 			t_content = f.read().strip('\n')
-		(self.host,self.port,self.database,self.user)= t_content.split(':')
-		
+		t_array = t_content.split(':')
+		if t_array[0] == 'postgresql': 
+			self.host = t_array[1]
+			self.port = t_array[2]
+			self.database = t_array[3]
+			self.user = t_array[4]
+		elif t_array[0] == 'mysql':
+			self.host = t_array[1]
+			self.port = t_array[2]
+			self.database = t_array[3]
+			self.user = t_array[4]
+			self.password = t_array[5]
+		elif t_array[0] == 'hive':
+			self.host = t_array[1]
+			self.port = t_array[2]
+			self.database = t_array[3]
+			self.user = t_array[4]
+			self.password = t_array[5]
+		elif t_array[0] == 'impala':
+			self.host = t_array[1]
+			self.port = t_array[2]
+			self.database = t_array[3]
+			self.user = t_array[4]
+			self.password = t_array[5]
+
+
 	def setDBC(self,DBC_FILE):
 		self.__dbc_file = DBC_FILE
-		with open(self.__dbc_file, 'rt') as f:
-			t_content = f.read().strip('\n')
-		(self.host,self.port,self.database,self.user)= t_content.split(':')
+		self.__getDBC(self)
+
+
